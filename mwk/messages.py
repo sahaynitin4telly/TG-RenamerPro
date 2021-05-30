f608f0ed94e35493db635f36106cdbd15203335b
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-05-30 21:36:06 +0530

    Nothing Official About it

---
+++ mwk/messages.py
@@ -0,0 +1,14 @@
+class Translation(object):
+    START_TEXT = """**Hello,
+This is a Telegram Rename Bot by @shamilnelli 😇
+
+Please send me any File, i can Rename It As Per Your choices to telegram as File/Video
+
+Hit /help for more details.. and more thing waiting for u there 😉**"""
+######################
+    HELP_USER = """**1 >>Send File/Video\n2 >>Choose Your desired Option\n3 >>Wait for it to process files**"""
+    DOWNLOAD_MSG = "**Yup Bro, Downloading....**😌"
+    DOWNLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
+    UPLOAD_MSG = "**Yeah Boy, Uploading it**🤓"
+    UPLOAD_FAIL_MSG = "**Sorry Baby 😔, Something Error With Your File**❎"
+    UPLOAD_DONE_MSG = "**Finally Uploaded Join My Update Channel.. if you like me 🤪"
