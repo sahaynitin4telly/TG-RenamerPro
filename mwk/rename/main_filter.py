c816d3019d7e4c7a6bf41759a6110423370ca84e
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-05-30 21:39:13 +0530

    Zero isnt enough

---
+++ mwk/rename/main_filter.py
@@ -0,0 +1,36 @@
+
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+@Client.on_message(filters.document | filters.video | filters.audio | filters.voice | filters.video_note | filters.animation) 
+async def rename_filter(c,m):
+  media = m.document or m.video or m.audio or m.voice or m.video_note or m.animation
+  text = ""
+  button = []
+  try:
+    filename = media.file_name
+    text += f"FileName:\n{filename}\n"
+  except:
+    # some files dont gib name ..
+    filename = None 
+    
+  text += "Select the desired Option"
+  button.append([InlineKeyboardButton("📂 Rename as File 📂", callback_data="rename_file")])
+  # Thanks to albert for mime_type suggestion 
+  if media.mime_type.startswith("video/"):
+    ## how the f the other formats can be uploaded as video 
+    button.append([InlineKeyboardButton("🎞️ Rename as Video 🎞️",callback_data="rename_video")])
+    button.append([InlineKeyboardButton("🎞️ Convert to File 📂",callback_data="convert_file")])
+    button.append([InlineKeyboardButton("📂 Convert to Video 🎞️",callback_data="convert_video")])
+  button.append([InlineKeyboardButton("Cancel ❌",callback_data="cancel")])
+ 
+  markup = InlineKeyboardMarkup(button)
+  try:
+    await m.reply_text(text,quote=True,reply_markup=markup,parse_mode="markdown",disable_web_page_preview=True)
+  except Exception as e:
+    log.info(str(e))
---
+++ mwk/rename/start_filters.py
@@ -0,0 +1,56 @@
+
+import os
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
+
+from pyrogram import Client,filters
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+from mwk.config import Config
+from mwk.messages import Translation
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+@Client.on_message(filters.command("help"))
+async def help_user(c,m):
+    try:
+       await m.reply_text(Translation.HELP_USER,quote=True)
+    except Exception as e:
+        log.info(str(e))
+        
+@Client.on_message(filters.command("start"))
+async def start_msg(c,m):
+    client.send_message(message.chat.id,
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+        parse_mode="markdown",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚙ Updates Channel", url=f"https://t.me/movieworldkdY")],
+                [
+                    InlineKeyboardButton(
+                        "👨‍🔬 Developer", url=f"https://t.me/shamilnelli"), 
+                    InlineKeyboardButton(
+                        "🛠 Support Group", url=f"https://t.me/redbullfed")
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+        )
+    try:
+       await m.reply_text(Translation.START_TEXT,quote=True,reply_markup=markup,disable_web_page_preview=True) 
+    except Exception as e:
+        log.info(str(e))
+
+        
+@Client.on_message(filters.command("log") & filters.private & filters.user(Config.OWNER_ID))
+async def log_msg(c,m):
+  z =await m.reply_text("Processing..", True)
+  if os.path.exists("Log.txt"):
+     await m.reply_document("Log.txt", True)
+     await z.delete()
+  else:
+    await z.edit_text("Log file not found")
