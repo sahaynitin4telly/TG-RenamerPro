7bb294bbf4cf93b3db54de0b46f89550ecfcad9c
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-03 14:07:58 +0530

    Update main_filter.py

--- mwk/rename/main_filter.py
+++ mwk/rename/main_filter.py
@@ -2,6 +2,14 @@
 import pyrogram
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from mwk.config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -9,6 +17,21 @@ log = logging.getLogger(__name__)
 
 @Client.on_message(filters.document | filters.video | filters.audio | filters.voice | filters.video_note | filters.animation) 
 async def rename_filter(c,m):
+  update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await c.get_chat_member(update_channel, m.chat.id)
+            if user.status == "kicked":
+               await m.reply_text("🤭 Sorry Dude, You are **B A N N E D**. If you feel You are not guilty please contact owner")
+               return
+        except UserNotParticipant:
+            await m.reply_text(
+                text="**Join My Updates Channel to use me & Enjoy the Free Service**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join Our Updates Channel", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+            return
   media = m.document or m.video or m.audio or m.voice or m.video_note or m.animation
   text = ""
   button = []
